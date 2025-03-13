77957209ee158a7180ca672e53543bda74b6f790
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-03-13 13:36:37 +0100

    added DOM function styleelement

--- scripts/modules/domUtils.js
+++ scripts/modules/domUtils.js
@@ -1,74 +1,80 @@
 //Basic Utilities
 export function createElement(tag, classNames = [], attributes = {}, textContent = "") {
-    const element = document.createElement(tag);
-    if (classNames.length) element.classList.add(...classNames);
-    Object.entries(attributes).forEach(([key, value]) => element.setAttribute(key, value));
-    element.textContent = textContent;
-    return element;
+  const element = document.createElement(tag);
+  if (classNames.length) element.classList.add(...classNames);
+  Object.entries(attributes).forEach(([key, value]) => element.setAttribute(key, value));
+  element.textContent = textContent;
+  return element;
 }
 
 export function appendChildren(parent, ...children) {
-    if (parent) children.forEach((child) => parent.appendChild(child));
+  if (parent) children.forEach((child) => parent.appendChild(child));
 }
 
 export function removeElement(element) {
-    element.remove();
+  element.remove();
 }
 
 export function removeClasses(element, classNames) {
-    if (classNames.length) element.classList.remove(...classNames);
+  if (classNames.length) element.classList.remove(...classNames);
 }
 
 export function addClasses(element, classNames = []) {
-    if (classNames.length) element.classList.add(...classNames);
+  if (classNames.length) element.classList.add(...classNames);
 }
 
 export function getElement(selector) {
-    return document.querySelector(selector);
+  return document.querySelector(selector);
 }
 
 export function getElements(selector) {
-    return document.querySelectorAll(selector);
+  return document.querySelectorAll(selector);
 }
 //-----------------------------------------------
 
 //List Utilities
 export async function createList(list, section) {
-    list.forEach((item) => {
-        const listItem = createListItem(item);
-        section.appendChild(listItem);
-    });
+  list.forEach((item) => {
+    const listItem = createListItem(item);
+    section.appendChild(listItem);
+  });
 }
 
 function createListItem(item) {
-    const listItem = createElement("div", ["list-item"]);
-    const rowOne = createElement("div", ["list-item__row"]);
-    const rowTwo = createElement("div", ["list-item__row"]);
+  const listItem = createElement("div", ["list-item"]);
+  const rowOne = createElement("div", ["list-item__row"]);
+  const rowTwo = createElement("div", ["list-item__row"]);
 
-    const itemName = createElement("h2", ["text-light"], {}, item.name);
-    const dottedLine = createElement("hr", ["list-item__hr", "text-light"], {});
-    const itemPrice = createElement("h2", ["text-light"], {}, item.price + " SEK");
+  const itemName = createElement("h2", ["text-light"], {}, item.name);
+  const dottedLine = createElement("hr", ["list-item__hr", "text-light"], {});
+  const itemPrice = createElement("h2", ["text-light"], {}, item.price + " SEK");
 
-    //Only for food menu. Needs work for receipts etc.
-    const itemInfo = createElement("h4", ["text-light"], {}, item.ingredients?.length ? item.ingredients.join(", ") : "");
-    const quantityButton = createElement("button", ["list-item__quantity-button"], {}, "Add");
+  //Only for food menu. Needs work for receipts etc.
+  const itemInfo = createElement("h4", ["text-light"], {}, item.ingredients?.length ? item.ingredients.join(", ") : "");
+  const quantityButton = createElement("button", ["list-item__quantity-button"], {}, "Add");
 
-    appendChildren(rowOne, itemName, dottedLine, itemPrice);
-    appendChildren(rowTwo, itemInfo, quantityButton);
-    appendChildren(listItem, rowOne, rowTwo);
+  appendChildren(rowOne, itemName, dottedLine, itemPrice);
+  appendChildren(rowTwo, itemInfo, quantityButton);
+  appendChildren(listItem, rowOne, rowTwo);
 
-    return listItem;
+  return listItem;
 }
 //-----------------------------------------------
 
 //Food Menu
 
 function checkType() {
-    //check item.type to sort by dish, dip or drink etc.
+  //check item.type to sort by dish, dip or drink etc.
 }
 
 function filterButtons() {
-    //create buttons for food menu filtering
+  //create buttons for food menu filtering
 }
 //-----------------------------------------------
 // h1 id="menuTitle" class="heading-one heading-one--light">Meny</h1
+
+//style utilities
+
+export function styleElement(element, property, color) {
+  element.style[property] = color;
+}
--- scripts/modules/utils.js
+++ scripts/modules/utils.js
@@ -1,4 +1,4 @@
-import { getElements, getElement, addClasses } from "./domUtils.js";
+import { getElements, getElement, addClasses, styleElement } from "./domUtils.js";
 import { getFromLocalStorage, removeFromLocalStorage, setLocalStorage } from "./localStorageUtils.js";
 
 export function getParams() {
@@ -19,14 +19,15 @@ export function checkParams(params) {
     startTimer(`#timerConfirmation`);
     confirmationSectionref.style.display = `flex`;
     addClasses(orderWrapperRef, [`d-none`]);
-    body.style.backgroundColor = `#605858`;
+    styleElement(body, `backgroundColor`, `#605858`);
+
     // Ovan är bara test. behöver komma åt variabelnamn från css
     // DOM funktion för att ändra style?
   }
   if (params.get(`showSingleReceipt`) === `true`) {
     addClasses(receiptwrapperRef, [`flex`]);
     addClasses(orderWrapperRef, [`d-none`]);
-    body.style.backgroundColor = `#605858`;
+    styleElement(body, `backgroundColor`, `#605858`);
 
     const startTime = getFromLocalStorage(`startTime`);
     if (startTime) {
@@ -38,7 +39,7 @@ export function checkParams(params) {
 export function startTimer(timerElementId) {
   const startTime = Date.now();
   setLocalStorage(`startTime`, startTime);
-
+  getConfirmationNumber(startTime);
   startCountdown(startTime, timerElementId);
 }
 
@@ -63,104 +64,6 @@ export function startCountdown(startTime, timerElementId) {
   }, 1000);
 }
 
-// export function startTimer(duration = 10 * 60) {
-//   console.log(`timer startad`);
-
-//   const id = `#${Date.now()}`;
-//   const countdownRefs = getElements(`.countdown`);
-
-//   let activeTimers = getFromLocalStorage(`activeTimers`);
-//   activeTimers.push({ id, timeLeft: duration });
-//   setLocalStorage(`activeTimers`, activeTimers);
-
-//   getConfirmationNumber(id);
-
-//   startCountdown(duration, id);
-// }
-
-// //Startar nedräkningen
-// export function startCountdown(duration, id) {
-//   let timeLeft = duration;
-//   const timers = {};
-
-//   timers[id] = setInterval(() => {
-//     timeLeft--;
-
-//     updateTimeLeftInStorage(id, timeLeft);
-
-//     if (timeLeft >= 0) {
-//       updateCountdown(timeLeft);
-//     }
-//     if (timeLeft === 0) {
-//       clearInterval(timers[id]);
-//       delete timers[id];
-//       finishCountdown(id);
-//     }
-//   }, 1000);
-// }
-
-// export function updateCountdown(timeLeft) {
-//   const countdownRefs = getElements(".countdown"); // Hämta alla element där timern ska visas
-//   const minutes = Math.floor(timeLeft / 60);
-//   const seconds = timeLeft % 60;
-//   countdownRefs.forEach((count) => {
-//     count.textContent = `Klar om ${minutes}m ${seconds}s`;
-//   });
-// }
-
-// export function finishCountdown(id) {
-//   const countdownRefs = getElements(".countdown");
-
-//   countdownRefs.forEach((count) => {
-//     count.textContent = `Maten är redo!`;
-//   });
-//   removeFromLocalStorage("activeTimers", id);
-// }
-
-// export function checkForActiveTimer() {
-//   const activeTimers = getFromLocalStorage(`activeTimers`);
-
-//   if (activeTimers.length > 0) {
-//     const id = activeTimers[activeTimers.length - 1].id;
-//     displayTimer(id);
-//   } else {
-//     console.log("Inga aktiva timers finns.");
-//   }
-// }
-
-// export function displayTimer(id) {
-//   const timeLeft = getTimer(id);
-
-//   if (timeLeft !== null) {
-//     if (timeLeft > 0) {
-//       updateCountdown(timeLeft);
-//     } else {
-//       finishCountdown(id);
-//     }
-//   } else {
-//     console.log(`Timer med id: ${id} finns inte i localeStorage`);
-//   }
-// }
-
 export function getConfirmationNumber(number) {
-  getElement(`#confirmationNumber`).textContent = number;
+  getElement(`#confirmationNumber`).textContent = `#${number}`;
 }
-
-// export function getTimer(id) {
-//   const activeTimers = getFromLocalStorage(`activeTimers`);
-//   const timer = activeTimers.find((timer) => timer.id === id);
-//   return timer ? timer.timeLeft : null;
-// }
-
-// export function updateTimeLeftInStorage(id, timeLeft) {
-//   let activeTimers = getFromLocalStorage(`activeTimers`);
-
-//   console.log("Aktiva timers före uppdatering:", activeTimers);
-
-//   const timerIndex = activeTimers.findIndex((timer) => timer.id === id);
-//   if (timerIndex !== -1) {
-//     activeTimers[timerIndex].timeLeft = timeLeft;
-//     console.log("Aktiva timers efter uppdatering:", activeTimers);
-//     setLocalStorage(`activeTimers`, activeTimers);
-//   }
-// }
